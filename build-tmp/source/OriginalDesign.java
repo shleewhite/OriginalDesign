d5d724893f9f9a460391093d6de8ab15d980ff88
shleewhite <lwhite@sportsbasement.com>
2014-08-22 10:17:06 -0700

    made original drawing

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -16,13 +16,103 @@ public class OriginalDesign extends PApplet {
 
 public void setup()
 {
-
+	size(400, 400);
 }
 public void draw()
 {
-  ellipse(50,50,50,50);
+ 	background();
+ 	body();
+ 	head();
+ 	straps();
 }
-
+public void background()
+{ background(97, 231, 255);
+    fill(234, 0, 255);
+    triangle(0,175,100,175,50,10);
+    fill(61, 255, 106);
+    rect(0,175,400,400);
+    noFill();
+    stroke(2, 133, 69);
+    strokeWeight(7);
+    ellipse(30,359,40,100);
+    fill(61, 255, 106);
+    noStroke();
+    rect(0,300,30,310);
+    noStroke();
+    fill(255, 0, 0);
+    ellipse(30,300,15,15);
+    ellipse(40,310,15,15);
+    ellipse(20,310,15,15);
+    ellipse(30,320,15,15);
+    fill(247, 255, 0);
+    ellipse(30,310,15,15);
+    noFill();
+    stroke(2, 133, 69);
+    strokeWeight(7);
+    ellipse(350,359,40,100);
+    noStroke();
+    fill(61, 255, 106);
+    rect(350,300,40,100);
+    fill(255, 0, 0);
+    ellipse(350,300,15,15);
+    ellipse(340,310,15,15);
+    ellipse(360,310,15,15);
+    ellipse(350,320,15,15);
+    fill(247, 255, 0);
+    ellipse(350,310,15,15);
+    }
+   
+public void body() 
+{
+    fill(84,84,255);
+    noStroke();
+    rect(115,200,150,180);
+    fill(52,71,237);
+    rect(115,376,150,30);
+    noFill();
+    strokeWeight(17);
+    stroke(84, 84, 255);
+    bezier(103,212,-71,40,126,89,157,24);
+    bezier(270,210,468,20,215,71,230,28);
+    fill(20, 222, 67);
+    noStroke();
+    rect(100,196,15,24);
+    rect(265,190,14,29);
+}
+    
+public void head()
+{
+    fill(255, 255, 255);
+   	rect(115,100,150,100);
+    fill(255, 243, 199);
+    ellipse(190,152,120,75);
+    fill(0, 0, 0);
+    ellipse(165,140,7,7);
+    ellipse(215,140,7,7);
+    fill(255, 243, 199);
+    stroke(0, 0, 0);
+    strokeWeight(3.5f);
+    ellipse(190,163,50,30);
+    noStroke();
+    ellipse(190,159,55,30);
+    fill(255, 255, 255);
+    ellipse(130,100,30,50);
+    ellipse(250,100,30,50);
+}
+    
+public void straps()
+{
+	fill(20, 222, 67);
+    ellipse(108,195,15,25);
+    rect(100,195,15,25);
+    rect(265,195,15,25);
+    ellipse(272,195,15,25);
+    fill(21, 173, 74);
+    rect(100,220,15,25);
+    ellipse(108,245,15,25);
+    rect(265,220,15,25);
+    ellipse(272,245,15,25);
+};
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
